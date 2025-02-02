47b3dd8808889e68fd40fa70cd5ee58a2033b265
Irafedoruk <irrnika@gmail.com>
2025-02-02 22:04:38 +0200

    fix add/remove product to cart

--- BackendShop/BackShop/Controllers/CartController.cs
+++ BackendShop/BackShop/Controllers/CartController.cs
@@ -21,30 +21,23 @@ public class CartController : ControllerBase
     }
 
     [HttpPost("add")]
-    public async Task<IActionResult> AddToCart([FromBody] List<AddToCartRequest> requests)
+    public async Task<IActionResult> Addtocart([FromBody] AddToCartRequest request)
     {
-        foreach (var item in requests)
+        Console.WriteLine($"Received userId: {request.UserId}, productId: {request.ProductId}, quantity: {request.Quantity}");
+        if (string.IsNullOrEmpty(request.UserId))
         {
-            Console.WriteLine($"Received userId: {item.UserId}, productId: {item.ProductId}, quantity: {item.Quantity}");
-            if (string.IsNullOrEmpty(item.UserId))
-            {
-                return Unauthorized("User is not authenticated");
-            }
-
-            await _cartService.AddToCartAsync(item.UserId, item.ProductId, item.Quantity);
+            return Unauthorized("User is not authenticated");
         }
 
-        return Ok(new { message = "Items added to cart successfully" });
+        await _cartService.AddToCartAsync(request.UserId, request.ProductId, request.Quantity);
+        return Ok(new { message = "Item added to cart successfully" });
     }
-    //public async Task<IActionResult> AddToCart([FromBody] AddToCartRequest request)
-    //{
-    //    Console.WriteLine($"Received userId: {request.UserId}, productId: {request.ProductId}, quantity: {request.Quantity}");
-    //    if (string.IsNullOrEmpty(request.UserId))
-    //    {
-    //        return Unauthorized("User is not authenticated");
-    //    }
 
-    //    await _cartService.AddToCartAsync(request.UserId, request.ProductId, request.Quantity);
-    //    return NoContent();
-    //}
+    [HttpDelete("remove/{userId}/{productId}")]
+    public async Task<IActionResult> RemoveFromCart(string userId, int productId)
+    {
+        await _cartService.RemoveFromCartAsync(userId, productId);
+        return Ok();
+    }
+
 }
