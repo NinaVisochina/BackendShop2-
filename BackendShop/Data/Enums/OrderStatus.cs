f83b91825ac4c16f4fc9006124bfb8ba82e11c9d
Irafedoruk <irrnika@gmail.com>
2025-02-11 10:29:16 +0200

    change order status by admin

--- BackendShop/Data/Enums/OrderStatus.cs
+++ BackendShop/Data/Enums/OrderStatus.cs
@@ -1,12 +1,5 @@
 ﻿namespace BackendShop.Data.Enums
 {
-    //public enum OrderStatus
-    //{
-    //    Pending,    // Очікується обробка
-    //    Processed,  // Оброблено
-    //    Delivered,  // Доставлено
-    //    Cancelled   // Скасовано
-    //}
     public enum OrderStatus
     {
         Pending = 0,
